efc755c1d2359bdd86716a5514b668739652084f
Kyle <hyperxgalaxy@gmail.com>
2021-08-06 01:13:03 -0400

    Updated main files
    .

--- RD3Project/src/rd3/RollerDerbyV3.java
+++ RD3Project/src/rd3/RollerDerbyV3.java
@@ -1,20 +1,3 @@
-//<<<<<<< HEAD
-///**
-// * Copyright (c) 2021
-// *
-// * RollerDerbyV3.java
-// * Created on Aug 6, 2021 12:58:54 AM
-//*/
-//package rd3;
-//
-//public class RollerDerbyV3 {
-//	
-//	public static void main(String[] args) {
-//		
-//		Hel
-//=======
-//package rd3;
-//
 //public class RollerDerbyV3 {
 //	public static void main(String[] args)
 //	{
@@ -27,6 +10,22 @@
 //				//if time to tick
 //					//update
 //					//collision checking
-//>>>>>>> b90555d1a2c82ff7389d5141aee75657783cb4cc
 //	}
 //}
+
+/**
+ * Copyright (c) 2021
+ *
+ * RollerDerbyV3.java
+ * Created on Aug 6, 2021 12:58:54 AM
+*/
+package rd3;
+
+public class RollerDerbyV3 {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+}
+
