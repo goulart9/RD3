9c899db52aa39469fbd78bdf06dc00e3ca62a8cc
Alex Scheufele <49530147+ascheufele@users.noreply.github.com>
2021-08-06 03:15:49 -0400

    Main Loop & RollerDerbyV3 Class

--- RD3Project/src/rd3/Map.java
+++ RD3Project/src/rd3/Map.java
@@ -5,9 +5,4 @@ public class Map {
 	int carColumn;
 	int[][] map;
 	public enum ROAD_STATE { EMPTY, OIL, POTHOLE, CONE }
-	
-	public static void main(String[] args) { 
-		
-		
-	}
 }
--- RD3Project/src/rd3/RollerDerbyV3.java
+++ RD3Project/src/rd3/RollerDerbyV3.java
@@ -1,10 +1,49 @@
 package rd3;
 
-public class RollerDerbyV3 {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.time.LocalTime;
+import javax.swing.JFrame;
+
+public class RollerDerbyV3 implements KeyListener{
+	JFrame frame;
+	//Map gameMap = new Map(8);
+	RollerDerbyV3() {
+		frame = new JFrame("Roller Derby V3");
+		frame.addKeyListener(this);
+		frame.setSize(800, 600);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 	
 	public static void main(String[] args) {
-		
-		
+		RollerDerbyV3 rd3 = new RollerDerbyV3();
+		int lastSecond = LocalTime.now().getSecond();
+		int currentSecond = 0;
+
+		while(true) {
+			//check if its time to step the map
+			currentSecond = LocalTime.now().getSecond();
+			if(lastSecond != currentSecond) {
+				System.out.println(LocalTime.now().getSecond());
+				//gameMap.step();
+				//gameMap.prettyPrint();
+				lastSecond = currentSecond;
+			}
+			
+		}
 	}
+	
+	public void keyPressed(KeyEvent e) {
+		int code = e.getKeyCode();
+		if(code == KeyEvent.VK_RIGHT) {
+			//gameMap.rightTurn();
+		}
+		if(code == KeyEvent.VK_LEFT) {
+			//gameMap.leftTurn();
+		}
+	}
+	public void keyTyped(KeyEvent e) {}
+	public void keyReleased(KeyEvent e) {}
 }
 
