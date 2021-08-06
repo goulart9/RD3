e89c3c66df74fe02e79b4b206db79d0aa2b4524a
Kyle <hyperxgalaxy@gmail.com>
2021-08-06 01:32:58 -0400

    Updated Java library
    .

--- RD3Project/src/rd3/Map.java
+++ RD3Project/src/rd3/Map.java
@@ -2,4 +2,12 @@ package rd3;
 
 public class Map {
 	
+	int carColumn;
+	int[][] map;
+	public enum ROAD_STATE { EMPTY, OIL, POTHOLE, CONE }
+	
+	public static void main(String[] args) { 
+		
+		
+	}
 }
